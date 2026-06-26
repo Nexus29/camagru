b50b45445b10d81d0a51d4e9fac531e2e9e8fb01
Nexus29 <giovanni.pio.lancellotta@gmail.com>
2026-06-26 10:25:22 +0200

    feat(editing): basic feature for the editing page

--- src/public/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "<h1>Camagru Mounting Test</h1>";
-echo "<p>Nginx successfully reached the PHP Entry Point!</p>";
---
+++ src/views/templates/footer.php
@@ -0,0 +1,6 @@
+</main> <footer class="global-footer">
+    <p>&copy; <?php echo date('Y'); ?> Camagru — 42 School Project. Framework-free implementation.</p>
+</footer>
+
+</body>
+</html>
